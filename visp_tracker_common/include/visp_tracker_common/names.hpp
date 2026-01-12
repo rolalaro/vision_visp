089170bd93313708f6cee0ab0f521f41899f52d5
Romain LAGNEAU <romain.lagneau@inria.fr>
2026-01-12 08:16:02 +0000

    [WIP] Creating new packages. Problems finding interface package

---
+++ visp_tracker_common/include/visp_tracker_common/names.hpp
@@ -0,0 +1,14 @@
+#ifndef NAMES_HPP
+#define NAMES_HPP
+
+#include <string>
+
+namespace visp_tracker_common
+{
+extern const std::string quit_srv_name; //!< Name of the service to quit the demos
+extern const std::string switch_vismode_srv_name; //!< Name of the service to activate / deactivate the visualization debug.
+
+extern const std::string features2D_topic_name; //!< Name of the topic on which are published 2D features to display in the remote GUI.
+extern const std::string poses_topic_name; //!< Name of the topic on which are published some poses of interest.
+}
+#endif
---
+++ visp_tracker_common/src/names.cpp
@@ -0,0 +1,10 @@
+#include <visp_tracker_common/names.hpp>
+
+namespace demo_utils
+{
+const std::string quit_srv_name = "/quit";
+const std::string switch_vismode_srv_name = "/switch_servoing_status";
+
+const std::string features2D_topic_name = "/features2D";
+const std::string poses_topic_name = "/poses";
+}
